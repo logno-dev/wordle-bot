c3211b21faf6b22e31baac8f691186b9066e9bfb
Logan Bunch <loganbunch@gmail.com>
2025-07-22 15:30:52 -0700

    Tue Jul 22 15:30:52 PDT 2025

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
-import { Database } from 'bun:sqlite'
-import { drizzle } from 'drizzle-orm/bun-sqlite'
-import { migrate } from 'drizzle-orm/bun-sqlite/migrator'
+import { createClient } from '@libsql/client'
+import { drizzle } from 'drizzle-orm/libsql'
+import { migrate } from 'drizzle-orm/libsql/migrator'
 import * as schema from './schema'
 import { join, dirname } from 'path'
 import { mkdirSync, existsSync } from 'fs'
@@ -13,8 +13,12 @@ if (!existsSync(dbDir)) {
   mkdirSync(dbDir, { recursive: true })
 }
 
-const sqlite = new Database(dbPath)
-export const db = drizzle(sqlite, { schema })
+// Create libsql client with local file
+const client = createClient({
+  url: `file:${dbPath}`
+})
+
+export const db = drizzle(client, { schema })
 
 export const initializeDatabase = () => {
   try {
